239dca00c2e62cefd63b872ff7f95ceee3318df7
thinh <21135681.thinh@student.iuh.edu.vn>
2025-11-15 02:59:15 +0700

    fix: add cors port 3000 of fe

--- src/main/java/iuh/fit/se/config/WebConfig.java
+++ src/main/java/iuh/fit/se/config/WebConfig.java
@@ -1,16 +1,16 @@
- package iuh.fit.se.config;
+package iuh.fit.se.config;
 
- import org.springframework.context.annotation.Configuration;
- import org.springframework.web.servlet.config.annotation.CorsRegistry;
- import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
- @Configuration
- public class WebConfig implements WebMvcConfigurer {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Địa chỉ frontend của bạn
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000") // Add multiple origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
- }
+}
