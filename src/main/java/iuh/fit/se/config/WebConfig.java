036cbea971185675975eb7c3bbc41d7611f6acab
thinh <21135681.thinh@student.iuh.edu.vn>
2025-11-24 00:50:39 +0700

    chore: add config cors url production of page admin

--- src/main/java/iuh/fit/se/config/WebConfig.java
+++ src/main/java/iuh/fit/se/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000","https://shoppingiuh.id.vn") // Add multiple origins
+                .allowedOrigins("http://localhost:5173", "http://localhost:3000","https://shoppingiuh.id.vn","https://admin.shoppingiuh.id.vn") // Add multiple origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
